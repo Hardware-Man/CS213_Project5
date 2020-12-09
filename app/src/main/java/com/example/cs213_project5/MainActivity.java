237c94df8befd731c87131feda6f57d9e10df533
Hardware-Man <kmishra686@gmail.com>
2020-12-08 23:56:55 -0500

    Activity updates

--- app/src/main/java/com/example/cs213_project5/MainActivity.java
+++ app/src/main/java/com/example/cs213_project5/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.cs213_project5;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button metSelect = findViewById(R.id.met_select);
+        Button naturalHistorySelect = findViewById(R.id.natural_history_select);
+        Button guggenheimSelect = findViewById(R.id.guggenheim_select);
+        Button modernArtSelect = findViewById(R.id.modern_art_select);
+
+        metSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+        naturalHistorySelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+        guggenheimSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+        modernArtSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+    }
+
+    private void launchActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cs213_project5/SecondActivity.java
+++ app/src/main/java/com/example/cs213_project5/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.cs213_project5;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Toast.makeText(getApplicationContext(),"Maximum of 5 tickets for each!",Toast.LENGTH_SHORT).show();
     }
 }
