5ec4944edac7eca52d8752225e7ec866b6a4941b
Hardware-Man <kmishra686@gmail.com>
2020-12-09 01:48:07 -0500

    Minor revisions / code cleanup

--- app/src/main/java/com/example/cs213_project5/SecondActivity.java
+++ app/src/main/java/com/example/cs213_project5/SecondActivity.java
@@ -1,8 +1,8 @@
 package com.example.cs213_project5;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -21,19 +21,36 @@ public class SecondActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),"Maximum of 5 tickets for each!",Toast.LENGTH_SHORT).show();
 
         ImageView museumPicture = findViewById(R.id.museumSelection);
+
+        TextView adultTicketPrice = findViewById(R.id.adultTicket);
+        TextView seniorTicketPrice = findViewById(R.id.seniorTicket);
+        TextView studentTicketPrice = findViewById(R.id.studentTicket);
+
         switch (MainActivity.selectedMuseum) {
             case "met":
                 museumPicture.setImageResource(R.drawable.met);
+                adultTicketPrice.setText(adultTicketPrice.getText() + " $25");
+                seniorTicketPrice.setText(seniorTicketPrice.getText() + " $17");
+                studentTicketPrice.setText(studentTicketPrice.getText() + " $12");
                 break;
             case "natural":
                 museumPicture.setImageResource(R.drawable.natural_history);
+                adultTicketPrice.setText(adultTicketPrice.getText() + " $23");
+                seniorTicketPrice.setText(seniorTicketPrice.getText() + " $18");
+                studentTicketPrice.setText(studentTicketPrice.getText() + " $18");
                 break;
             case "guggenheim":
                 museumPicture.setImageResource(R.drawable.guggenheim);
+                adultTicketPrice.setText(adultTicketPrice.getText() + " $25");
+                seniorTicketPrice.setText(seniorTicketPrice.getText() + " $18");
+                studentTicketPrice.setText(studentTicketPrice.getText() + " $18");
                 break;
             default:
                 museumPicture.setImageResource(R.drawable.modern_art);
+                adultTicketPrice.setText(adultTicketPrice.getText() + " $25");
+                seniorTicketPrice.setText(seniorTicketPrice.getText() + " $14");
+                studentTicketPrice.setText(studentTicketPrice.getText() + " $18");
         }
-        museumPicture.setOnClickListener(v -> startActivity(new Intent(this, SecondActivity.class)));
+        museumPicture.setOnClickListener(v -> startActivity(new Intent(this, ThirdActivity.class)));
     }
 }
